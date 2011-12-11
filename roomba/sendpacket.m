a3e1162d8e829229ceaf8d638eccddcc5af765ad
ty244@cornell.edu <ty244@cornell.edu@f1c21082-6dad-d30a-7b78-f86e6ae89214>
2011-12-11 21:28:14 +0000

    allow msg to have a payload

--- roomba/sendpacket.m
+++ roomba/sendpacket.m
@@ -5,24 +5,25 @@ rh='192.168.1.141';
 port=8866;
 disp(msg);
 packetType = 0;
-if (strcmp(msg,'INIT') == 1)
+if (strcmp(msg(0),'INIT') == 1)
     packetType = 1;
-elseif (strcmp(msg,'END') == 1)
+elseif (strcmp(msg(0),'END') == 1)
     packetType = 2;
-elseif (strcmp(msg,'CTRL') == 1)
+elseif (strcmp(msg(0),'CTRL') == 1)
     packetType = 3;
-elseif (strcmp(msg,'DATA') == 1)
+elseif (strcmp(msg(0),'DATA') == 1)
     packetType = 4;
-elseif (strcmp(msg,'ERROR') == 1)
+elseif (strcmp(msg(0),'ERROR') == 1)
     packetType = 5;
-elseif (strcmp(msg,'SHUTDOWN') == 1)
+elseif (strcmp(msg(0),'SHUTDOWN') == 1)
     packetType = 6;
 else
     packetType = 7;
 end
+packet = [packetType msg(1:end)];
 %get UDP packet from the remote host
 u=udp(rh,port,'LocalPort',port);
 fopen(u);
-fwrite(u,packetType);
+fwrite(u,packet);
 fclose(u);
 end
