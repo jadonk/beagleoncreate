c9d3c937905745932837a2dd0ff8e282bb2f38aa
yangchuck@gmail.com <yangchuck@gmail.com@f1c21082-6dad-d30a-7b78-f86e6ae89214>
2012-02-01 20:36:59 +0000

    move doc into its own branch, instead of trunk

---
+++ doc/html/files.js
@@ -0,0 +1,18 @@
+var files =
+[
+    [ "D:/Chuck/Desktop/beagleoncreate/main.cpp", "main_8cpp.html", "main_8cpp" ],
+    [ "D:/Chuck/Desktop/beagleoncreate/Packet.h", "_packet_8h.html", "_packet_8h" ],
+    [ "D:/Chuck/Desktop/beagleoncreate/camera/ARtag.cpp", "_a_rtag_8cpp.html", null ],
+    [ "D:/Chuck/Desktop/beagleoncreate/camera/ARtag.h", "_a_rtag_8h.html", null ],
+    [ "D:/Chuck/Desktop/beagleoncreate/camera/ARtagLocalizer.cpp", "_a_rtag_localizer_8cpp.html", "_a_rtag_localizer_8cpp" ],
+    [ "D:/Chuck/Desktop/beagleoncreate/camera/ARtagLocalizer.h", "_a_rtag_localizer_8h.html", "_a_rtag_localizer_8h" ],
+    [ "D:/Chuck/Desktop/beagleoncreate/camera/Camera.cpp", "_camera_8cpp.html", "_camera_8cpp" ],
+    [ "D:/Chuck/Desktop/beagleoncreate/camera/Camera.h", "_camera_8h.html", "_camera_8h" ],
+    [ "D:/Chuck/Desktop/beagleoncreate/control/Control.h", "_control_8h.html", "_control_8h" ],
+    [ "D:/Chuck/Desktop/beagleoncreate/create/Create.cpp", "_create_8cpp.html", "_create_8cpp" ],
+    [ "D:/Chuck/Desktop/beagleoncreate/create/Create.h", "_create_8h.html", "_create_8h" ],
+    [ "D:/Chuck/Desktop/beagleoncreate/sonar/Gpio.cpp", "_gpio_8cpp.html", "_gpio_8cpp" ],
+    [ "D:/Chuck/Desktop/beagleoncreate/sonar/Gpio.h", "_gpio_8h.html", "_gpio_8h" ],
+    [ "D:/Chuck/Desktop/beagleoncreate/sonar/Sonar.cpp", "_sonar_8cpp.html", "_sonar_8cpp" ],
+    [ "D:/Chuck/Desktop/beagleoncreate/sonar/Sonar.h", "_sonar_8h.html", "_sonar_8h" ]
+];
