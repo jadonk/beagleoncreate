10c6f6a46ac62b32e4789fe83395d2dca05f227e
yangchuck@gmail.com <yangchuck@gmail.com@f1c21082-6dad-d30a-7b78-f86e6ae89214>
2012-02-01 20:23:17 +0000

    adding beagleOnCreate.png adding the doxygen documentations

---
+++ doc/html/search/functions_69.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['initartagpose',['initARtagPose',['../class_a_rtag_localizer.html#a869a7090148876255844713d8fde85e2',1,'ARtagLocalizer']]],
+  ['initserial',['InitSerial',['../class_create.html#acc906e7c65f7b91f011adaa45bf652be',1,'Create']]],
+  ['isbroadcast',['isBroadcast',['../class_camera.html#adbda12e714701f9711e1d0159202c28d',1,'Camera']]]
+];
